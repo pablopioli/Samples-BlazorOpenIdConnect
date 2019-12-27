83549493d56b0f7e8100037756fb6ce921f352d8
Pablo Pioli <ppioli@outlook.com>
2019-12-27 18:10:22 -0300

    Popup authentication works

---
+++ RedirectMiddleware.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
+
+namespace WebApplication1
+{
+    public class RedirectMiddleware : IMiddleware
+    {
+        public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+        {
+            //if (context.Request.Path.StartsWithSegments(new PathString("/oidc/callbacks")))
+            //{
+            //    var redirect = "?p=" + context.Request.Path +
+            //                   "&q=" + context.Request.QueryString.ToString().Substring(1).Replace("&", "~and~");
+            //    context.Response.Redirect(redirect);
+            //    return;
+            //}
+            await next(context);
+        }
+    }
+}
